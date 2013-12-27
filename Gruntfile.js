f710275c1a878afba135180840666f923f0394bd
Douglas <douglaslise@gmail.com>
2013-12-27 11:14:11 -0200

    Disabled htmlmin grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -409,8 +409,8 @@ module.exports = function (grunt) {
         'copy:dist',
         'modernizr',
         'rev',
-        'usemin',
-        'htmlmin'
+        'usemin'/*,
+        'htmlmin'*/
     ]);
 
     grunt.registerTask('default', [
